4ebc9c9f93c0ffaa13038a7f090243ae81e8e9d5
Jan Figura <janfig27@gmail.com>
2022-06-11 21:18:02 +0200

    feat: Return jwt on user creation

--- src/main/java/com/example/warresourcesapi/controller/UserController.java
+++ src/main/java/com/example/warresourcesapi/controller/UserController.java
@@ -1,19 +1,32 @@
 package com.example.warresourcesapi.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.warresourcesapi.model.AppUser;
 import com.example.warresourcesapi.model.Role;
 import com.example.warresourcesapi.model.request.RoleToUserRequest;
 import com.example.warresourcesapi.model.request.UserCreateRequest;
 import com.example.warresourcesapi.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -27,9 +40,31 @@ public class UserController {
     }
 
     @PostMapping("/user/save")
-    public ResponseEntity<AppUser> createUser(@RequestBody AppUser user) {
+    public void createUser(@RequestBody AppUser user,
+                                              HttpServletRequest request,
+                                              HttpServletResponse response
+    ) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString());
-        return ResponseEntity.created(uri).body(userService.saveUser(user));
+        AppUser savedUser = userService.saveUser(user);
+
+        // TODO: Zroibć utilty dla tworzenia tokena
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("Secret".getBytes());
+            String access_token = JWT.create()
+                    .withSubject(String.valueOf(savedUser.getId()))
+                    .withIssuer(request.getRequestURI())
+                    .withClaim("roles", savedUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                    .sign(algorithm);
+            Map<String, String> token = new HashMap<>();
+            token.put("Authorization", "Bearer " + access_token);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        return ResponseEntity.created(uri).body();
     }
 
     @PostMapping("/role/save")
