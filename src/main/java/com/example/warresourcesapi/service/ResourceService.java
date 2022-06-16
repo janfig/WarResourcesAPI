e0a29e6c8e81c671c03aa58c83b5df872f3291ac
Jan Figura <janfig27@gmail.com>
2022-06-16 16:51:33 +0200

    fix: Deleted fetch service from resource

--- src/main/java/com/example/warresourcesapi/service/ResourceService.java
+++ src/main/java/com/example/warresourcesapi/service/ResourceService.java
@@ -8,8 +8,6 @@ import com.example.warresourcesapi.utils.FileDownloader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -28,28 +26,6 @@ public class ResourceService {
 
     public ResourceService(ResourceRepository resourceRepository) {
         this.resourceRepository = resourceRepository;
-    }
-
-
-    public void fetchRecords() throws IOException, InterruptedException {
-        Resource resource = new Resource("gold");
-        Resource resource2 = new Resource("oil");
-        logger.info("Resources repository empty! Downloading resources");
-        FileDownloader.download(
-                FileDownloader.redirectLink("https://www.kaggle.com/api/v1/datasets/download/omdatas/historic-gold-prices"),
-                "gold.zip");
-        logger.info("Unzipping file");
-        FileDownloader.unzip(FileDownloader.getResPath(), "gold.zip");
-        ArrayList<String[]> arrayList = csvToArray(FileDownloader.getResPath(), "goldx.csv");
-        ArrayList<Price> prices = arrayToPrices(arrayList);
-        HashSet<Price> prices2 = new HashSet<>();
-        prices2.add(new Price(22.22, LocalDate.of(20, 2, 2)));
-        logger.info("Saving resource to repository");
-        resource.setPrices(new HashSet<>(prices));
-        resource2.setPrices(prices2);
-//        System.out.println(resource);
-        resourceRepository.save(resource);
-        resourceRepository.save(resource2);
     }
 
     public List<Resource> getResources() {
